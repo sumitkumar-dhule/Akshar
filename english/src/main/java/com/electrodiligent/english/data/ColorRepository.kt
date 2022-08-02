081ebd5f264fb3b18cc87a4ea3db6d442d3c9c4b
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-02 23:19:05 +0530

    Add Colour Repository

---
+++ english/src/main/java/com/electrodiligent/english/data/ColorRepository.kt
@@ -0,0 +1,19 @@
+package com.electrodiligent.english.data
+
+import androidx.compose.ui.graphics.Color
+import com.electrodiligent.core.domain.model.ColorItem
+
+object ColorRepository {
+    val list = listOf(
+       ColorItem("RED", Color.Red),
+       ColorItem("YELLOW", Color.Yellow),
+       ColorItem("GREEN", Color.Green),
+       ColorItem("WHITE", Color.White),
+       ColorItem("BLUE", Color.Blue),
+       ColorItem("ORANGE", Color.Black),
+       ColorItem("BLACK", Color.Black),
+       ColorItem("PURPLE", Color.Red),
+       ColorItem("BROWN", Color.Red),
+       ColorItem("PINK", Color.Red),
+    )
+}
