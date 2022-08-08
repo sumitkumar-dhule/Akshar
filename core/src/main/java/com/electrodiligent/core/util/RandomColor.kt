0432053382eeff0294f48d85551af303f8879b63
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-08 21:04:17 +0530

    Ui updates

--- core/src/main/java/com/electrodiligent/core/util/RandomColor.kt
+++ core/src/main/java/com/electrodiligent/core/util/RandomColor.kt
@@ -8,18 +8,10 @@ object RandomColor {
         ColorItem("RED", Color.Red),
         ColorItem("GREEN", Color.Green),
         ColorItem("BLUE", Color.Blue),
-        ColorItem(
-            "ORANGE",
-            Color(red = 255, green = 165, blue = 0)
-        ),
+        ColorItem("ORANGE", Color(red = 255, green = 165, blue = 0)),
         ColorItem("BLACK", Color.Black),
-        ColorItem(
-            "PURPLE",
-            Color(red = 128, green = 0, blue = 128)
-        ),
-        ColorItem(
-            "BROWN",
-            Color(red = 168, green = 42, blue = 42)
-        )
+        ColorItem("PURPLE", Color(red = 128, green = 0, blue = 128)),
+        ColorItem("BROWN", Color(red = 168, green = 42, blue = 42)),
+        ColorItem("PINK", Color(red = 255, green = 0, blue = 127))
     )
 }
