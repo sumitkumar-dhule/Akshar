db02f0a4800003d8b767e73e2dfb8ec9a1bf60ca
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-17 10:08:57 +0530

    Reduce app size

--- english/src/main/java/com/electrodiligent/english/data/BirdsRepository.kt
+++ english/src/main/java/com/electrodiligent/english/data/BirdsRepository.kt
@@ -7,91 +7,93 @@ object BirdsRepository {
     val list = listOf(
         PictureItem(
             name = "CRANE", imageId = R.drawable.crane,
-            //     audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.crane
         ),
         PictureItem(
             "CROW",
             R.drawable.crow,
-//            audio = com.electrodiligent.english.R.raw.circle
+            audio = com.electrodiligent.english.R.raw.crow
         ),
         PictureItem(
             name = "EAGLE",
             imageId = R.drawable.eagle,
-//            audio = com.electrodiligent.english.R.raw.oval
+            audio = com.electrodiligent.english.R.raw.eagle
         ),
         PictureItem(
             "FLAMINGO",
             R.drawable.flamingo,
-//            audio = com.electrodiligent.english.R.raw.triangle
+            audio = com.electrodiligent.english.R.raw.flamingo
         ),
         PictureItem(
             "HAWK",
             R.drawable.hawk,
-//            audio = com.electrodiligent.english.R.raw.square
+            audio = com.electrodiligent.english.R.raw.hawk
         ),
         PictureItem(
             "OWL",
             R.drawable.owl,
-//            audio = com.electrodiligent.english.R.raw.square
+            audio = com.electrodiligent.english.R.raw.owl
         ),
         PictureItem(
             "PARROT",
             R.drawable.parrot,
-//            audio = com.electrodiligent.english.R.raw.square
+            audio = com.electrodiligent.english.R.raw.parrot
         ),
         PictureItem(
             "HEN",
             R.drawable.hen,
 
-//            audio = com.electrodiligent.english.R.raw.rectangle
+            audio = com.electrodiligent.english.R.raw.hen
         ),
         PictureItem(
             "HORN BILL",
             R.drawable.hornbill,
-//            audio = com.electrodiligent.english.R.raw.diamond
+            audio = com.electrodiligent.english.R.raw.hornbill
         ),
         PictureItem(
             "HUMMING BIRD",
             R.drawable.humming_bird,
-//            audio = com.electrodiligent.english.R.raw.pentagon
+            audio = com.electrodiligent.english.R.raw.humming_bird
         ),
         PictureItem(
             "KINGFISHER",
             R.drawable.kingfisher,
-//            audio = com.electrodiligent.english.R.raw.hexagon
+            audio = com.electrodiligent.english.R.raw.kingfisher
         ),
         PictureItem(
             name = "OSTRICH",
             imageId = R.drawable.ostrich,
-//            audio = com.electrodiligent.english.R.raw.star
+            audio = com.electrodiligent.english.R.raw.ostrich
         ),
         PictureItem(
             name = "PEACOCK", imageId = R.drawable.peacock,
-//            audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.peacock
         ),
         PictureItem(
             name = "PENGUIN", imageId = R.drawable.penguin,
-//            audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.penguin
         ),
         PictureItem(
             name = "SWAN", imageId = R.drawable.swan,
-//            audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.swan
         ),
         PictureItem(
             name = "TURKEY", imageId = R.drawable.turkey,
-//            audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.turkey
         ),
         PictureItem(
             name = "WOODPECKER", imageId = R.drawable.woodpecker,
-//            audio = com.electrodiligent.english.R.raw.heart
+            audio = com.electrodiligent.english.R.raw.woodpecker
         ),
         PictureItem(
-            name = "PIGEON", imageId = R.drawable.pigeon,
-//            audio = com.electrodiligent.english.R.raw.heart
-        ), PictureItem(
             name = "KIWI",
             imageId = R.drawable.kiwi_bird,
-//            audio = com.electrodiligent.english.R.raw.star
+            audio = com.electrodiligent.english.R.raw.kiwi_bird
+        ),
+        PictureItem(
+            name = "PIGEON",
+            imageId = R.drawable.pigeon,
+            audio = com.electrodiligent.english.R.raw.pigeon
         )
 
     )
