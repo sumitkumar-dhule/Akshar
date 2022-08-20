8c79d08f53054e84fee932d2895a723c606044a4
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-20 17:23:15 +0530

    Add translation for vyanjan

--- hindi/src/main/java/com/electrodiligent/hindi/data/VyanjanRepository.kt
+++ hindi/src/main/java/com/electrodiligent/hindi/data/VyanjanRepository.kt
@@ -2,156 +2,231 @@ package com.electrodiligent.hindi.data
 
 import com.electrodiligent.core.domain.model.DisplayCharacter
 import com.electrodiligent.core.R.drawable.*
+import com.electrodiligent.hindi.R
 
 object VyanjanRepository {
     val list = listOf(
         DisplayCharacter(
             left = "श्र", display = "क", right = "ख",
-             displayImage = pigeon
+            displayImage = pigeon,
+            displayName = "कबूतर",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "क", display = "ख", right = "ग",
-             displayImage = rabbit
+            displayImage = rabbit,
+            displayName = "खरगोश",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ख", display = "ग", right = "घ",
-             displayImage = donkey
+            displayImage = donkey,
+            displayName = "गधा",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ग", display = "घ", right = "ङ",
-             displayImage = horse
+            displayImage = horse,
+            displayName = "घोडा",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "घ", display = "ङ", right = "च",
-             displayImage = daa
+            displayImage = daa,
+            displayName = "",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ङ", display = "च", right = "छ",
-             displayImage = spoon
+            displayImage = spoon,
+            displayName = "चम्मच",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "च", display = "छ", right = "ज",
-             displayImage = umrella
+            displayImage = umrella,
+            displayName = "छाता",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "छ", display = "ज", right = "झ",
-             displayImage = jug
+            displayImage = jug,
+            displayName = "जग",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ज", display = "झ", right = "ञ",
-             displayImage = indian_flag
+            displayImage = indian_flag,
+            displayName = "झंडा",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "झ", display = "ञ", right = "ट",
-             displayImage = tra
+            displayImage = tra,
+            displayName = "",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ञ", display = "ट", right = "ठ",
-             displayImage = tomato
+            displayImage = tomato,
+            displayName = "टमाटर",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ट", display = "ठ", right = "ड",
-             displayImage = thela
+            displayImage = thela,
+            displayName = "ठेला",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ठ", display = "ड", right = "ढ",
-             displayImage = damaru
+            displayImage = damaru,
+            displayName = "डमरू",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ड", display = "ढ", right = "ण",
-             displayImage = dhol
+            displayImage = dhol,
+            displayName = "ढोल",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ढ", display = "ण", right = "त",
-             displayImage = arrow
+            displayImage = arrow,
+            displayName = "बाण",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ण", display = "त", right = "थ",
-             displayImage = watermelon
+            displayImage = watermelon,
+            displayName = "तरबूज",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "त", display = "थ", right = "द",
-             displayImage = thermos
+            displayImage = thermos,
+            displayName = "थर्मस",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "थ", display = "द", right = "ध",
-             displayImage = medicine
+            displayImage = medicine,
+            displayName = "दवाई",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "द", display = "ध", right = "न",
-             displayImage = bow
+            displayImage = bow,
+            displayName = "धनुष",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ध", display = "न", right = "प",
-             displayImage = lemon
+            displayImage = lemon,
+            displayName = "नींबू",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "न", display = "प", right = "फ",
-             displayImage = kite
+            displayImage = kite,
+            displayName = "पतंग",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "प", display = "फ", right = "ब",
-             displayImage = football
+            displayImage = football,
+            displayName = "फुटबॉल",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "फ", display = "ब", right = "भ",
-             displayImage = duck
+            displayImage = duck,
+            displayName = "",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ब", display = "भ", right = "म",
-             displayImage = bear
+            displayImage = bear,
+            displayName = "बतख",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "भ", display = "म", right = "य",
-             displayImage = fish
+            displayImage = fish,
+            displayName = "मछली",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "म", display = "य", right = "र",
-             displayImage = yadnya
+            displayImage = yadnya,
+            displayName = "यज्ञ",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "य", display = "र", right = "ल",
-             displayImage = queen_ashwini
+            displayImage = queen_ashwini,
+            displayName = "रानी",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "र", display = "ल", right = "व",
-             displayImage = garlic
+            displayImage = garlic,
+            displayName = "लहसुन",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ल", display = "व", right = "श",
-             displayImage = forest
+            displayImage = forest,
+            displayName = "वन",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "व", display = "श", right = "ष",
-             displayImage = ostrich
+            displayImage = ostrich,
+            displayName = "शुतुरमुर्ग",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "श", display = "ष", right = "स",
-             displayImage = hexagon
+            displayImage = hexagon,
+            displayName = "षट्कोण",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ष", display = "स", right = "ह",
-             displayImage = apple
+            displayImage = apple,
+            displayName = "सेब",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "स", display = "ह", right = "क्ष",
-             displayImage = deer
+            displayImage = deer,
+            displayName = "हिरन",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ह", display = "क्ष", right = "त्र",
-            displayImage = kshatriya
+            displayImage = kshatriya,
+            displayName = "क्षत्रिय",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "क्ष", display = "त्र", right = "ज्ञ",
-             displayImage = trishul
+            displayImage = trishul,
+            displayName = "त्रिशूल",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "त्र", display = "ज्ञ", right = "श्र",
-             displayImage = dnyaneshar
+            displayImage = dnyaneshwar,
+            displayName = "ज्ञानी",
+            audio = R.raw.beep
         ),
         DisplayCharacter(
             left = "ज्ञ", display = "श्र", right = "क",
-            displayImage = shramik
+            displayImage = shramik,
+            displayName = "श्रमिक",
+            audio = R.raw.beep
         )
     )
 }
