2dc4bafbb337ab73e03e3329f4e41dca4a8f2aa1
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-20 09:04:58 +0530

    Fix wrong refactoring

--- core/src/main/java/com/electrodiligent/core/navigation/Screen.kt
+++ core/src/main/java/com/electrodiligent/core/navigation/Screen.kt
@@ -2,6 +2,8 @@ package com.electrodiligent.core.navigation
 
 sealed class Screen(val route: String) {
     object HomeScreen : Screen("home_screen")
+    object AlphabetIdentificationScreen : Screen("alphabet_identification_screen")
+    object AlphabetFlashcardScreen : Screen("alphabet_flashcard_screen")
     object PracticeScreen : Screen("practice_screen")
     object SwarIdentificationScreen : Screen("swar_identification_screen")
     object VyanjanIdentificationScreen : Screen("vyanjan_identification_screen")
