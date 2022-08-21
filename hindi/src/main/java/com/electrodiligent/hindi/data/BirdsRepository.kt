be35f05461fee9939964c6838766c86445b940b7
Sumitkumar <sumitkumarDhule@gmail.com>
2022-08-21 12:48:16 +0530

    Add bird sounds

--- hindi/src/main/java/com/electrodiligent/hindi/data/BirdsRepository.kt
+++ hindi/src/main/java/com/electrodiligent/hindi/data/BirdsRepository.kt
@@ -10,84 +10,82 @@ object BirdsRepository {
         PictureItem(
             "कौआ",
             R.drawable.crow,
-           //            audio = com.electrodiligent.english.R.raw.crow
+            audio = com.electrodiligent.hindi.R.raw.kauaa
         ),
         PictureItem(
             name = "गरुड़",
             imageId = R.drawable.eagle,
-           //            audio = com.electrodiligent.english.R.raw.eagle
+            audio = com.electrodiligent.hindi.R.raw.garud
         ),
         PictureItem(
             "राजहंस",
             R.drawable.flamingo,
-           //            audio = com.electrodiligent.english.R.raw.flamingo
+            audio = com.electrodiligent.hindi.R.raw.rajhans
         ),
         PictureItem(
             "बाज",
             R.drawable.hawk,
-           //            audio = com.electrodiligent.english.R.raw.hawk
+            audio = com.electrodiligent.hindi.R.raw.baaz
         ),
         PictureItem(
             "उल्लू",
             R.drawable.owl,
-           //            audio = com.electrodiligent.english.R.raw.owl
+            audio = com.electrodiligent.hindi.R.raw.ullu
         ),
         PictureItem(
             "तोता",
             R.drawable.parrot,
-           //            audio = com.electrodiligent.english.R.raw.parrot
+            audio = com.electrodiligent.hindi.R.raw.tota
         ),
         PictureItem(
             "मुर्गी",
             R.drawable.hen,
-
-           //            audio = com.electrodiligent.english.R.raw.hen
+            audio = com.electrodiligent.hindi.R.raw.murgi
         ),
         PictureItem(
             "हमिंग बर्ड",
             R.drawable.humming_bird,
-           //            audio = com.electrodiligent.english.R.raw.humming_bird
+            audio = com.electrodiligent.hindi.R.raw.hummingbird
         ),
         PictureItem(
             "नीलकंठ",
             R.drawable.kingfisher,
-           //            audio = com.electrodiligent.english.R.raw.kingfisher
+            audio = com.electrodiligent.hindi.R.raw.nilkanth
         ),
         PictureItem(
             name = "शुतुरमुर्ग",
             imageId = R.drawable.ostrich,
-           //            audio = com.electrodiligent.english.R.raw.ostrich
+            audio = com.electrodiligent.hindi.R.raw.shururmurg
         ),
         PictureItem(
             name = "मोर", imageId = R.drawable.peacock,
-           //            audio = com.electrodiligent.english.R.raw.peacock
+            audio = com.electrodiligent.hindi.R.raw.mor
         ),
         PictureItem(
             name = "पेंगुइन", imageId = R.drawable.penguin,
-           //            audio = com.electrodiligent.english.R.raw.penguin
+            audio = com.electrodiligent.hindi.R.raw.pengine
         ),
         PictureItem(
             name = "हंस", imageId = R.drawable.swan,
-           //            audio = com.electrodiligent.english.R.raw.swan
+            audio = com.electrodiligent.hindi.R.raw.hans
         ),
         PictureItem(
             name = "टर्की", imageId = R.drawable.turkey,
-           //            audio = com.electrodiligent.english.R.raw.turkey
+            audio = com.electrodiligent.hindi.R.raw.turkey
         ),
         PictureItem(
             name = "कठफोड़वा", imageId = R.drawable.woodpecker,
-           //            audio = com.electrodiligent.english.R.raw.woodpecker
+            audio = com.electrodiligent.hindi.R.raw.kathfodwa
         ),
         PictureItem(
             name = "कीवी",
             imageId = R.drawable.kiwi_bird,
-           //            audio = com.electrodiligent.english.R.raw.kiwi_bird
+            audio = com.electrodiligent.hindi.R.raw.kiwi
         ),
         PictureItem(
             name = "कबूतर",
             imageId = R.drawable.pigeon,
-           //            audio = com.electrodiligent.english.R.raw.pigeon
+            audio = com.electrodiligent.hindi.R.raw.kabutar
         )
-
     )
 }
