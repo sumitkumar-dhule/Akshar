8add0241e18f1112ea8ffcf29bbb10311c60b2d5
Sumitkumar <sumitkumarDhule@gmail.com>
2025-12-13 20:50:40 +0530

    feat: Change AppBar title as per screen content

--- english/src/main/java/com/electrodiligent/english/presentation/MainActivity.kt
+++ english/src/main/java/com/electrodiligent/english/presentation/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -47,6 +50,29 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val scope = rememberCoroutineScope()
                 val drawerState = rememberDrawerState(DrawerValue.Closed)
+                val currentTitle = remember { mutableStateOf("Preschool Essentials") }
+
+                LaunchedEffect(navController) {
+                    navController.currentBackStackEntryFlow.collect { backStackEntry ->
+                        currentTitle.value = when (backStackEntry.destination.route) {
+                            Screen.HomeScreen.route -> "Preschool Essentials"
+                            Screen.PracticeScreen.route -> "Practice Zone"
+                            Screen.AlphabetIdentificationScreen.route -> "Identify Alphabets"
+                            Screen.AlphabetFlashcardScreen.route -> "Alphabet Flashcards"
+                            Screen.NumberIdentificationScreen.route -> "Numbers"
+                            Screen.NumberFlashcardScreen.route -> "Number Flashcards"
+                            Screen.ShapesScreen.route -> "Shapes"
+                            Screen.ColorsScreen.route -> "Colors"
+                            Screen.VegetablesScreen.route -> "Vegetables"
+                            Screen.FruitsScreen.route -> "Fruits"
+                            Screen.AnimalsScreen.route -> "Animals"
+                            Screen.BirdsScreen.route -> "Birds"
+                            // Add all other screens...
+                            else -> "Preschool Essentials" // Default title
+                        }
+                    }
+                }
+
 
                 ModalNavigationDrawer(
                     drawerState = drawerState,
@@ -72,7 +98,7 @@ class MainActivity : ComponentActivity() {
                         contentWindowInsets = WindowInsets.safeDrawing,
                         topBar = {
                             AppBar(
-                                title = "Preschool Essentials",
+                                title = currentTitle.value,
                                 onNavigationIconClick = {
                                     scope.launch { drawerState.open() }
                                 }
